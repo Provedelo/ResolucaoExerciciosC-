0c2583fa28d9d8f5516eeabe2c969bdc5ac225bf
Provedelo <103537863+Provedelo@users.noreply.github.com>
2022-04-12 19:11:55 -0300

    Add files via upload
    Calculadora básica para treino em C#

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Calculadora
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnMultiplicacao = new System.Windows.Forms.Button();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.txbNumero1 = new System.Windows.Forms.TextBox();
+            this.txbNumero2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(199, 12);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(30, 23);
+            this.btnSoma.TabIndex = 0;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnMultiplicacao
+            // 
+            this.btnMultiplicacao.Location = new System.Drawing.Point(199, 65);
+            this.btnMultiplicacao.Name = "btnMultiplicacao";
+            this.btnMultiplicacao.Size = new System.Drawing.Size(30, 23);
+            this.btnMultiplicacao.TabIndex = 1;
+            this.btnMultiplicacao.Text = "*";
+            this.btnMultiplicacao.UseVisualStyleBackColor = true;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.btnMultiplicacao_Click);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.Location = new System.Drawing.Point(235, 12);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(30, 23);
+            this.btnSubtracao.TabIndex = 2;
+            this.btnSubtracao.Text = "-";
+            this.btnSubtracao.UseVisualStyleBackColor = true;
+            this.btnSubtracao.Click += new System.EventHandler(this.btnSubtracao_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Location = new System.Drawing.Point(235, 65);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(30, 23);
+            this.btnDivisao.TabIndex = 3;
+            this.btnDivisao.Text = "/";
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // txbNumero1
+            // 
+            this.txbNumero1.Location = new System.Drawing.Point(80, 9);
+            this.txbNumero1.Name = "txbNumero1";
+            this.txbNumero1.Size = new System.Drawing.Size(100, 23);
+            this.txbNumero1.TabIndex = 4;
+            // 
+            // txbNumero2
+            // 
+            this.txbNumero2.Location = new System.Drawing.Point(80, 38);
+            this.txbNumero2.Name = "txbNumero2";
+            this.txbNumero2.Size = new System.Drawing.Size(100, 23);
+            this.txbNumero2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Valor 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Valor 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultado:";
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.Location = new System.Drawing.Point(80, 70);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.ReadOnly = true;
+            this.txbResultado.Size = new System.Drawing.Size(100, 23);
+            this.txbResultado.TabIndex = 9;
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 108);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbNumero2);
+            this.Controls.Add(this.txbNumero1);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.btnMultiplicacao);
+            this.Controls.Add(this.btnSoma);
+            this.Name = "Calculadora";
+            this.Text = "Calculadora - TreinaWeb";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnMultiplicacao;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.TextBox txbNumero1;
+        private System.Windows.Forms.TextBox txbNumero2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbResultado;
+    }
+}
+
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Calculadora : Form
+    {
+     
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+  /*      abstract void Checagem();
+         try
+        {
+        StringComparer check = new StringComparer(txbNumero1.Text, txbNumero2.Text);
+        string numero;
+        while ((numero = StringComparer.ReadLine()) != null && != char) 
+            {
+            return (void)
+            }
+        }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro desconhecido: " + ex.Message);
+            }
+return void */
+
+
+private void btnSoma_Click(object sender, EventArgs e) 
+        {
+            int numero1 = Convert.ToInt32(txbNumero1.Text);
+            int numero2 = Convert.ToInt32(txbNumero2.Text);
+            txbResultado.Text = Somar(numero1, numero2).ToString();
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+            int numero1 = Convert.ToInt32(txbNumero1.Text);
+            int numero2 = Convert.ToInt32(txbNumero2.Text);
+            txbResultado.Text = Subtrair(numero1, numero2).ToString();
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            int numero1 = Convert.ToInt32(txbNumero1.Text);
+            int numero2 = Convert.ToInt32(txbNumero2.Text);
+            txbResultado.Text = Multiplicar(numero1, numero2).ToString();
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            float numero1 = Convert.ToInt32(txbNumero1.Text);
+            float numero2 = Convert.ToInt32(txbNumero2.Text);
+            txbResultado.Text = Dividir(numero1, numero2).ToString();
+        }
+
+        int Somar(params int[] numeros) //usado para adicionar inumeros valores para somar
+        {
+            int resultado = 0;
+            foreach (int numero in numeros)
+            {
+                resultado += numero;
+            }
+
+            return resultado;
+        }
+        
+        int Subtrair (params int[] numeros)
+        {
+            int resultado = 0;
+            foreach (int numero in numeros)
+            {
+                resultado = -resultado - numero;
+            }
+            return resultado;
+        }
+        int Multiplicar(params int[] numeros)
+        {
+            int resultado = 1;
+            foreach (int numero in numeros)
+            {
+                resultado = resultado * numero;
+            }
+            return resultado;
+        }
+        float Dividir(params float[] numeros) 
+        {
+            float resultado = 0F;
+            foreach (int numero in numeros)
+            {
+                if (resultado == 0)
+                {
+                    resultado = numero;
+                }else
+             resultado = resultado / numero; 
+            
+            }
+            return resultado;
+
+     /* private void btnSoma_Click(object sender, EventArgs e) //com conotação normal
+       {
+           int numero1 = Convert.ToInt32(txbNumero1.Text);
+           int numero2 = Convert.ToInt32(txbNumero2.Text);
+           int resultado = numero1 + numero2;
+           txbResultado.Text =Convert.ToString(resultado);
+       }
+
+       private void btnSubtracao_Click(object sender, EventArgs e)
+       {
+           int numero1 = Convert.ToInt32(txbNumero1.Text);
+           int numero2 = Convert.ToInt32(txbNumero2.Text);
+           int resultado = numero1 - numero2;
+           txbResultado.Text = Convert.ToString(resultado);
+       }
+
+       private void btnMultiplicacao_Click(object sender, EventArgs e)
+       {
+           int numero1 = Convert.ToInt32(txbNumero1.Text);
+           int numero2 = Convert.ToInt32(txbNumero2.Text);
+           int resultado = numero1 * numero2;
+           txbResultado.Text = Convert.ToString(resultado);
+       }
+
+       private void btnDivisao_Click(object sender, EventArgs e)
+       {
+           float numero1 = Convert.ToInt32(txbNumero1.Text);
+           float numero2 = Convert.ToInt32(txbNumero2.Text);
+           float resultado = numero1 / numero2;
+           txbResultado.Text = Convert.ToString(resultado);
+       }*/
+        }
+    }
+}
