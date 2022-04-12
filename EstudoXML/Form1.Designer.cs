693bc958d2a5d904f8e9219d9d0fa0c8b0587dd8
Provedelo <103537863+Provedelo@users.noreply.github.com>
2022-04-12 19:18:16 -0300

    Add files via upload
    Teste de gerenciamento de dados em XML

---
+++ EstudoXML/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace EstudoXML
+{
+    partial class Agenda
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxContatos = new System.Windows.Forms.ListBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxContatos
+            // 
+            this.lbxContatos.FormattingEnabled = true;
+            this.lbxContatos.ItemHeight = 15;
+            this.lbxContatos.Location = new System.Drawing.Point(12, 56);
+            this.lbxContatos.Name = "lbxContatos";
+            this.lbxContatos.Size = new System.Drawing.Size(367, 259);
+            this.lbxContatos.TabIndex = 1;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.Location = new System.Drawing.Point(12, 18);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(61, 22);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "label1";
+            // 
+            // Agenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 326);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lbxContatos);
+            this.Name = "Agenda";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Agenda_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lbxContatos;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ EstudoXML/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace EstudoXML
+{
+    public partial class Agenda : Form
+    {
+        public Agenda()
+        {
+            InitializeComponent();
+        }
+
+        private void Agenda_Load(object sender, EventArgs e)
+        {
+            CriarContato();
+            lblTitulo.Text = CarregarTitulo();
+            CarregarContatos();
+        }
+        private void CarregarContatos()
+        {
+            XmlDocument documentoXml = new XmlDocument();
+            documentoXml.Load(@"\\vmware-host\Shared Folders\Desenvolvimento\Projetos e Estudos\Estudo Treina Web\C#\Exercicios\TreinaWebPleno\CalculadoraDelegate\EstudoXML\Agenda.xml");//Adicionado o @ para o caracter especial \ que mostra o caminho do diretório "nó"
+            XmlNodeList nodeContato = documentoXml.SelectNodes("/agenda/contatos/contato"); //atravez do caminho xpath retornar uma espressao ou nó, onde /agenda/titulo seleciona o nó que vc deseja no caso o nó do titulo
+            foreach(XmlNode contato in nodeContato) //node de nó -> nóContato
+            {
+                string representarContato = "";
+                string id = contato.Attributes["id"].Value;
+                string nome = contato.Attributes["nome"].Value;
+                string idade = contato.Attributes["idade"].Value;
+                representarContato = nome +", "+ idade + ", "+id;
+                lbxContatos.Items.Add(representarContato);
+            }
+            
+        }
+        private string CarregarTitulo()
+        {
+            XmlDocument documentoXml = new XmlDocument();
+            documentoXml.Load(@"\\vmware-host\Shared Folders\Desenvolvimento\Projetos e Estudos\Estudo Treina Web\C#\Exercicios\TreinaWebPleno\CalculadoraDelegate\EstudoXML\Agenda.xml");//Adicionado o @ para o caracter especial \ que mostra o caminho do diretório "nó"
+            XmlNode noTitulo = documentoXml.SelectSingleNode("/agenda/titulo"); //atravez do caminho xpath retornar uma espressao ou nó, onde /agenda/titulo seleciona o nó que vc deseja no caso o nó do titulo
+            return noTitulo.InnerText;
+        }
+        private void CriarContato()
+        {
+            XmlDocument documentoXml = new XmlDocument();
+            documentoXml.Load(@"\\vmware-host\Shared Folders\Desenvolvimento\Projetos e Estudos\Estudo Treina Web\C#\Exercicios\TreinaWebPleno\CalculadoraDelegate\EstudoXML\Agenda.xml");//Adicionado o @ para o caracter especial \ que mostra o caminho do diretório "nó"
+            
+            XmlAttribute atributoId = documentoXml.CreateAttribute("id"); //cria-se os atributos para o elemento
+            atributoId.Value = "teste 5";
+
+            XmlAttribute atributoNome = documentoXml.CreateAttribute("nome");
+            atributoNome.Value = "teste jose";
+
+            XmlAttribute atributoIdade = documentoXml.CreateAttribute("idade");
+            atributoIdade.Value = "teste 25";
+
+            XmlNode novoContato = documentoXml.CreateElement("contato"); //criase o nó contato para ser passado os atributos
+            novoContato.Attributes.Append(atributoId);// faz o vinculo do atributo ao nó contato
+            novoContato.Attributes.Append(atributoNome);
+            novoContato.Attributes.Append(atributoIdade);
+           
+            XmlNode contatos = documentoXml.SelectSingleNode("/agenda/contatos");//seleciona o nó a ser inserido a informação criada
+            contatos.AppendChild(novoContato);//adiciona o filho ao nó
+
+            documentoXml.Save(@"\\vmware-host\Shared Folders\Desenvolvimento\Projetos e Estudos\Estudo Treina Web\C#\Exercicios\TreinaWebPleno\CalculadoraDelegate\EstudoXML\Agenda.xml");//vai salvar no documento as alterações feitas
+
+        }
+    }
+}
